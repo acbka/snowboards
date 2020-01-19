c63dde3d4086cfbf537554c74cc56005313b52f8
acbka <vladochka.vb@gmail.com>
2020-01-19 19:10:18 +1300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// background
+function backgroundImage(){
+   const backgrounds = [
+      "./img/camada3.png",
+      "./img/camada4.png"
+   ]
+   const interval = 3000;
+   let currentImage = 0;
+
+   if (currentImage == 0){
+      
+   }
+}
